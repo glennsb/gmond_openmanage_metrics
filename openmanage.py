5a8e9966404829f203e80c43c852129f4d6e5c79
Stuart Glenn <Stuart-Glenn@omrf.org>
2011-12-13 11:25:23 -0600

    start of a crappy temp monitor first

---
+++ openmanage.py
@@ -0,0 +1,53 @@
+import subprocess
+import sys
+import os
+
+descriptors = list()
+om_path = "/opt/dell/srvadmin/bin/omreport"
+ambient_index = "0"
+
+def System_Board_Ambient(name):
+    global om_path
+    global ambient_index
+    p1 = subprocess.Popen([om_path,"chassis","temps","Index="+ambient_index],stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(["grep","Reading"],stdin=p1.stdout,stdout=subprocess.PIPE)
+    p3 = subprocess.Popen(["awk","{print $3}"],stdin=p2.stdout,stdout=subprocess.PIPE)
+    temp = (p3.communicate()[0]).rstrip(os.linesep)
+    return float(temp)
+    
+def metric_init(params):
+    global descriptors
+    global om_path
+    global ambient_index
+    
+    if 'om_path' in params:
+        om_path = params['om_path']
+
+    if 'ambient_index' in params:
+        ambient_index = params['ambient_index']
+    
+    d1 = {'name': 'System_Board_Ambient',
+        'call_back': System_Board_Ambient,
+        'time_max': 90,
+        'value_type': 'float',
+        'units': 'Celsius',
+        'slope': 'both',
+        'format': '%f',
+        'description': 'System board ambient temperature probe',
+        'groups': 'hardware'}
+
+    descriptors = [d1]
+    return descriptors
+
+def metric_cleanup():
+    '''Clean up the metric module.'''
+    pass
+
+#This code is for debugging and unit testing    
+if __name__ == '__main__':
+    params = {'ambient_index': '0',
+        'om_path': '/opt/dell/srvadmin/bin/omreport'}
+    metric_init(params)
+    for d in descriptors:
+        v = d['call_back'](d['name'])
+        print 'value for %s is %u' % (d['name'],  v)
